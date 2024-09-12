5e2d723ca335e9875393cc15b1e7a77028fadf0f
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-12 09:51:16 -0400

    Time: 5581 ms (47.02%), Space: 19 MB (25.58%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,47 +1,45 @@
-# We are using the TrieNode Data Structure
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.isWord = False 
+        self.end = False
+    
+    def addWord(self, w):
+        cur = self
 
-    def addWord(self, word):
-        cur = self 
-        for c in word:
+        for c in w:
             if c not in cur.children:
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
-        cur.isWord = True 
+        cur.end = True
+
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         root = TrieNode()
         for w in words:
             root.addWord(w)
-
-        ROWS, COLS = len(board), len(board[0])
+            
+        R, C = len(board), len(board[0])
         res, visit = set(), set()
 
         def dfs(r, c, node, word):
-            if (r < 0 or c<0 or r == ROWS or c == COLS or (r,c) in visit or board[r][c] not in node.children):
-                return
-
+            if (r < 0 or c < 0 or r == R or c == C or 
+            board[r][c] not in node.children or (r,c) in visit):
+                return 
             visit.add((r,c))
-            # node represents what node we are currently at 
             node = node.children[board[r][c]]
             word += board[r][c]
-
-            if node.isWord:
+            if node.end:
                 res.add(word)
-
-            dfs(r + 1, c, node, word)
-            dfs(r - 1, c, node, word)
-            dfs(r, c + 1, node, word)
-            dfs(r, c - 1, node, word)
+            dfs(r+1, c, node, word)
+            dfs(r-1, c, node, word)
+            dfs(r, c+1, node, word)
+            dfs(r, c-1, node, word)
             visit.remove((r,c))
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                dfs(r,c,root,"")
-
-        return list(res)
         
+        for r in range(R):
+            for c in range(C):
+                dfs(r, c, root, "")
+        return list(res)
+
+
